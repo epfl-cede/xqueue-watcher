a45cc600beb90f2d7650afa50fb4509fba080319
Oleg Demakov <oleg.demakov@epfl.ch>
2019-02-12 14:46:04 +0100

    separate config for staging and campus

--- testGrader/test.py
+++ testGrader/test.py
@@ -2,6 +2,7 @@ from xqueue_watcher.grader import Grader
 
 class testGrader(Grader):
     def grade(self, grader_path, grader_config, student_response):
+        print("student_response={}".format(student_response))
         result_0 = {
             'errors': [],
             'tests': [],
